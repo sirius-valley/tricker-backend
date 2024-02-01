aa23139f1d2a9b7af8ea307fec207b154af4dae5
unknown <juanbianchi@icloud.com>
2024-02-01 18:10:36 -0300

    install swagger and set configuration in server

---
+++ src/utils/swagger.ts
@@ -0,0 +1,21 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+const options = {
+    definition: {
+        openapi: "3.1.0",
+        info: {
+            title: "Tricker API with Swagger",
+            version: "0.1.0",
+            description:
+                "This is a Restful API application made with Express and documented with Swagger",
+        },
+        servers: [
+            {
+                url: "http://localhost:8080",
+            },
+        ],
+    },
+    apis: ["./docs/swagger.json"],
+};
+
+export const specs = swaggerJsdoc(options);
