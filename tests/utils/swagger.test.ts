3a05d31c8a2c5992560955fad78ad204dd7831e3
Lucas David Cardozo <lucascardozo@sirius.com.ar>
2024-02-02 12:18:17 -0300

    add commands and fix config for testing ts files

--- tests/utils/swagger.test.ts
+++ tests/utils/swagger.test.ts
@@ -2,10 +2,9 @@ import { describe, it } from "node:test"
 import axios from 'axios';
 import assert from "node:assert"
 
-describe("/api-docs", () => {
+describe("documentation", () => {
     it("should return status code 200", async (_t) : Promise<void> => {
-        const res = await axios.get('http://localhost:8080/api-docs/');
-
-        assert.equal(res.status, 200);
+        const res = await axios.get('http://localhost:8080/api-docs')
+        assert.equal(200, res.status)
     })
 })
