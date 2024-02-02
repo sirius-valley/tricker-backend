71d7712af46d3689cdc3ae8ddf46bca5ff7597a5
Lucas David Cardozo <lucascardozo@sirius.com.ar>
2024-02-02 08:40:30 -0300

    improve validation mechanism

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -4,18 +4,20 @@ import { ValidationException } from "./errors";
 import { plainToInstance } from "class-transformer";
 
 export type ClassType<T> = { new(...args: any[]): T; }
+export type RequestProp = 'body' | 'query' | 'params';
 
-export function BodyValidation<T>(target: ClassType<T>) {
+export function validateRequest<T>(target: ClassType<T>, reqProp: RequestProp) {
     return async (req: Request, res: Response, next: NextFunction) => {
-        req.body = plainToInstance(target, req.body);
-        const errors = await validate(req.body, {
+        const instance = plainToInstance(target, req[reqProp]);
+        const errors = await validate(instance as any, {
             whitelist: true,
-            forbidNonWhitelisted: true,
+            forbidNonWhitelisted: true
         });
 
         if (errors.length > 0)
             throw new ValidationException(errors.map(error => ({ ...error, target: undefined, value: undefined })));
 
+        req[reqProp] = instance;
         next();
     }
 }
