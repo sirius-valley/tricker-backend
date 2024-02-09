a7730a2d561033e530348f6f8f9fd81cdf4cb633
Juan <juanbianchi@sirius.com.ar>
2024-02-09 11:40:42 -0300

    modify swagger endpoints, add user router to express router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,7 @@
+import { userRouter } from '@domains/user/controller';
+import { withAuth } from '@utils/auth';
 import { Router } from 'express';
 
 export const router = Router();
+
+router.use('/user', withAuth, userRouter);
