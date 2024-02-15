bffec07b0eb7c4032715ab51aedd8b53e548f318
Lucas David Cardozo <lucascardozo@sirius.com.ar>
2024-02-15 07:35:38 -0300

    merge

--- src/domains/user/repository/user.repository.ts
+++ src/domains/user/repository/user.repository.ts
@@ -5,4 +5,5 @@ export interface UserRepository {
   create: (data: SignupInputDTO) => Promise<UserModel>;
   getById: (id: string) => Promise<UserDTO | null>;
   getByEmailOrUsername: (email?: string, username?: string) => Promise<UserModel | null>;
+  getByProviderId: (providerId: string) => Promise<UserModel | null>;
 }
