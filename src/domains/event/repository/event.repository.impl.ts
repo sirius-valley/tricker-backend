4736721b05c789f7ee5447c9938a92ad4ab48b35
Lucas David Cardozo <lucascardozo@sirius.com.ar>
2024-02-24 15:58:10 -0300

    add correct return dtos

--- src/domains/event/repository/event.repository.impl.ts
+++ src/domains/event/repository/event.repository.impl.ts
@@ -1,35 +1,47 @@
 import { type EventRepository } from '@domains/event/repository/event.repository';
-import { type ChangeScalarEventInput, type IssueChangeLogDTO, type BlockEventInput, type BlockerStatusModificationDTO } from '../dto';
+import { type ChangeScalarEventInput, IssueChangeLogDTO, type BlockEventInput, BlockerStatusModificationDTO } from '../dto';
 import type { PrismaClient } from '@prisma/client';
 
 export class EventRepositoryImpl implements EventRepository {
   constructor(private readonly db: PrismaClient) {}
 
-  async createIssueChangeLog(event: ChangeScalarEventInput): Promise<IssueChangeLogDTO> {
-    return await this.db.issueChangeLog.create({
+  async createIssueChangeLog(input: ChangeScalarEventInput): Promise<IssueChangeLogDTO> {
+    const event = await this.db.issueChangeLog.create({
       data: {
-        providerEventId: event.providerEventId,
-        userEmitterId: event.userEmitterId,
-        issueId: event.issueId,
-        eventRegisteredAt: event.createdAt,
-        field: event.field,
-        from: event.from?.toString(),
-        to: event.to?.toString(),
+        providerEventId: input.providerEventId,
+        userEmitterId: input.userEmitterId,
+        issueId: input.issueId,
+        eventRegisteredAt: input.createdAt,
+        field: input.field,
+        from: input.from?.toString(),
+        to: input.to?.toString(),
       },
+    });
+
+    return new IssueChangeLogDTO({
+      ...event,
+      from: event.from ?? undefined,
+      to: event.to ?? undefined,
+      eventRegisteredAt: event.eventRegisteredAt ?? undefined,
     });
   }
 
-  async createIssueBlockEvent(event: BlockEventInput): Promise<BlockerStatusModificationDTO> {
-    return await this.db.blockerStatusModification.create({
+  async createIssueBlockEvent(input: BlockEventInput): Promise<BlockerStatusModificationDTO> {
+    const event = await this.db.blockerStatusModification.create({
       data: {
-        providerEventId: event.providerEventId,
-        issueId: event.issueId,
-        userEmitterId: event.userEmitterId,
-        eventRegisteredAt: event.createdAt,
-        status: event.type,
-        reason: event.reason,
-        comment: event.comment,
+        providerEventId: input.providerEventId,
+        issueId: input.issueId,
+        userEmitterId: input.userEmitterId,
+        eventRegisteredAt: input.createdAt,
+        status: input.type,
+        reason: input.reason,
+        comment: input.comment,
       },
+    });
+
+    return new BlockerStatusModificationDTO({
+      ...event,
+      eventRegisteredAt: event.eventRegisteredAt ?? undefined,
     });
   }
 }
