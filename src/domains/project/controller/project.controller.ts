a6c1d7fa7a2f1b68dc0247e8a84cbf0cdead7a7e
Juan <juanbianchi@sirius.com.ar>
2024-02-27 11:05:36 -0300

    small changes after merge

--- src/domains/project/controller/project.controller.ts
+++ src/domains/project/controller/project.controller.ts
@@ -6,7 +6,6 @@ import { LinearAdapter } from '@domains/adapter/linearAdapter/linear.adapter';
 import { type ProjectManagementTool } from '@domains/adapter/projectManagementTool';
 import { type ProjectRepository, ProjectRepositoryImpl } from '@domains/project/repository';
 import { type CustomCognitoIdTokenPayload, type UserRepository, UserRepositoryImpl } from '@domains/user';
-import { type RoleRepository, RoleRepositoryImpl } from '@domains/role/repository';
 import HttpStatus from 'http-status';
 import { type PendingAuthProjectRepository, PendingAuthProjectRepositoryImpl } from '@domains/pendingAuthProject/repository';
 require('express-async-errors');
@@ -15,10 +14,9 @@ export const projectRouter: Router = Router();
 
 const projectRepository: ProjectRepository = new ProjectRepositoryImpl(db);
 const userRepository: UserRepository = new UserRepositoryImpl(db);
-const roleRepository: RoleRepository = new RoleRepositoryImpl(db);
 const pendingAuthRepository: PendingAuthProjectRepository = new PendingAuthProjectRepositoryImpl(db);
 const projectTool: ProjectManagementTool = new LinearAdapter(pendingAuthRepository);
-const service: ProjectServiceImpl = new ProjectServiceImpl(projectTool, projectRepository, userRepository, roleRepository);
+const service: ProjectServiceImpl = new ProjectServiceImpl(projectTool, projectRepository, userRepository);
 
 projectRouter.post('/integration/linear', async (req: Request, res: Response) => {
   // TO DO: See how data is sent to the endpoint. Another endpoint?
