9c7b9a5e8c69f8b0542bdaea650e94859cab3deb
f-pochat <federico.pochat@ing.austral.edu.ar>
2024-03-13 15:34:40 -0300

    Fix cors

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use(cookieParser()); // Parse cookies
 // Set up CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
