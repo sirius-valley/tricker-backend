0d382bf681eba93a2477d77bb79a2a12c8495177
Lucas David Cardozo <lucascardozo@sirius.com.ar>
2024-03-20 10:49:39 -0300

    add prisma seeding

---
+++ .lintstagedrc.js
@@ -0,0 +1,16 @@
+// lint-staged.config.js
+const { ESLint } = require('eslint');
+
+const removeIgnoredFiles = async (files) => {
+  const eslint = new ESLint();
+  const ignoredFiles = await Promise.all(files.map((file) => eslint.isPathIgnored(file)));
+  const filteredFiles = files.filter((_, i) => !ignoredFiles[i]);
+  return filteredFiles.join(' ');
+};
+
+module.exports = {
+  '*.{js,ts}': async (files) => {
+    const filesToLint = await removeIgnoredFiles(files);
+    return ['prettier --write .', `eslint --fix --max-warnings=0 ${filesToLint}`];
+  },
+};
